7aa50946f53e9f91b522828164259e2a01b4dc8a
Dyniek <damian.kawerski@gmail.com>
2025-07-11 14:57:40 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/core";
+// import { useState } from "react";
+// import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 
 function App() {
